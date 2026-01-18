eef809847848e295c5feb806c42bf098f889b74a
anupam458 <anupam800942@gmail.com>
2026-01-18 11:24:22 +0530

    Update number_guess_game.py

--- newfile.py
+++ newfile.py
@@ -1,3 +1,20 @@
-name = input("Enter your name = ")
-age = input("Enter your age = ")
-print("hello",name, "you are",age, "years old")
+import random
+
+secret_number = random.randint(1, 10)
+count = 0
+
+while True:
+    try:
+        guess = int(input("Guess the number between 1 to 10 = "))
+        count += 1
+
+        if guess > secret_number:
+            print("Too high!")
+        elif guess < secret_number:
+            print("Too low!")
+        else:
+            print("Correct guess, you won 🎉")
+            print("You took", count, "attempts")
+            break
+    except ValueError:
+        print("Please enter a valid number.")
